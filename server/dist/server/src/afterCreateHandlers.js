9d2cc9099264b28a9f7897bdac585dfa6e706d0c
Leon-101 <Leon_101@foxmail.com>
2025-05-04 14:25:51 +0800

    Remove server/dist/

--- server/dist/server/src/afterCreateHandlers.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.openvscode = openvscode;
-const child_process_1 = require("child_process");
-function openvscode(basePath, filePath) {
-    (0, child_process_1.execSync)(`code ${basePath}`);
-    (0, child_process_1.execSync)(`code ${filePath}`);
-}
